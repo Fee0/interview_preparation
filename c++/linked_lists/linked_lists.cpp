16bd0b07d538f0ec6b8a6df2020a898188e89b2c
Frederik Peukert <frederik.peukert@gmx.de>
2017-08-23 02:39:13 +0200

    adding functionality

--- c++/linked_lists/linked_lists.cpp
+++ c++/linked_lists/linked_lists.cpp
@@ -30,15 +30,28 @@ public:
 			tmp->value = 123;
 			_back = tmp;
 			tmp = tmp->p;
+			this->_size++;
+		}
+	}
+
+	ll(const ll& other)
+	{
+		entry* tmp = other->_front;
+		while (tmp) {
+			
+			
+
+			tmp = tmp->p;
 		}
 	}
 
 	void print() {
 		entry* tmp = _front;
 		while (tmp) {
-			std::cout << tmp->value << std::endl;
+			std::cout << tmp->value << " ";
 			tmp = tmp->p;
 		}
+		std::cout << std::endl;
 	}
 
 	void push_back(T v) {
@@ -53,10 +66,59 @@ public:
 			_back->p->p = NULL;
 			_back->p->value = v;
 			_back = _back->p;
-
 		}
 
+		this->_size++;
 		assert(_back->p == NULL);
+	}
+
+	void push_front(T v) {
+		if (_front == NULL) {
+			_front = new(entry);
+			_front->p = NULL;
+			_front->value = v;
+			_back = _front;
+
+		} else {
+			entry* tmp = new(entry);
+			tmp->p = _front;
+			tmp->value = v;
+			_front = tmp;
+		}
+
+		this->_size++;
+		assert(_back->p == NULL);
+	}
+
+	int size() {
+		return _size;
+	}
+
+	T& operator[] (unsigned x) {
+		assert(x <= this->_size);
+
+		entry* tmp = _front;
+		unsigned idx = 0;
+		while (tmp) {
+			if (idx == x)
+				return tmp->value;
+			idx++;
+			tmp = tmp->p;
+		}
+	}
+
+	~ll() {
+		entry* tmp = _front;
+		while (tmp) {
+			entry* tmp2 = tmp->p;
+			delete tmp;
+			tmp = tmp2;
+		}
+	}
+
+	ll<T>& operator= (const ll<T>& other)
+	{
+		return *this;
 	}
 };
 
@@ -65,6 +127,11 @@ int main() {
 	mylist.push_back(1);
 	mylist.push_back(2);
 	mylist.push_back(3);
+	mylist.push_front(4);
+	mylist.push_front(5);
+	mylist.push_front(6);
 	mylist.print();
-	std::cout << "hello" << std::endl;
+
+	assert(mylist[2] == 4);
+	assert(mylist.size() == 6);
 }
